3aa28986cd9c5f65f88f070b89a7aca511ad04fb
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-06-24 20:46:58 -0300

    efeito de seleção

--- battleborne/script.js
+++ battleborne/script.js
@@ -1,67 +1,69 @@
 var tipoProfissao = "";
 
-const guerreiro = document.querySelector("#warrior")
-const mago = document.querySelector("#mage")
-const arqueiro = document.querySelector("#archer")
+const guerreiro = document.querySelector("#warrior");
+const mago = document.querySelector("#mage");
+const arqueiro = document.querySelector("#archer");
 
 function alteraValorProfissao(valor) {
-    tipoProfissao = valor;
+  tipoProfissao = valor;
 }
 
-function calculaSoma() {
-    const range1 =  document.getElementById("ataque");
-    const range2 =  document.getElementById("vida");
-    const range3 =  document.getElementById("defesa");
+guerreiro.addEventListener("click", function() {
+  alteraValorProfissao("Guerreiro");
+  activateButton(guerreiro);
+});
 
-    var range1Value = parseInt(range1.value);
-    var range2Value = parseInt(range2.value);
-    var range3Value = parseInt(range3.value);
-    
-    var sum = range1Value + range2Value + range3Value;
-    
-    if (sum > 10) {
-        window.alert("Soma maior que 10");
-        range1.value = 0;
-        range2.value = 0;
-        range3.value = 0;
-        document.getElementById("sum").textContent = "Soma: " + 0;
-    }
-    else
-    {
-        document.getElementById("sum").textContent = "Soma: " + sum;
-    }  
+mago.addEventListener("click", function() {
+  alteraValorProfissao("Mago");
+  activateButton(mago);
+});
+
+arqueiro.addEventListener("click", function() {
+  alteraValorProfissao("Arqueiro");
+  activateButton(arqueiro);
+});
+
+var characterButtons = document.getElementsByClassName('character-button');
+
+for (var i = 0; i < characterButtons.length; i++) {
+  characterButtons[i].addEventListener('click', function() {
+    activateButton(this);
+  });
 }
 
-guerreiro.addEventListener("click", () => alteraValorProfissao("Guerreiro"));
-mago.addEventListener("click", () => alteraValorProfissao("Mago"));
-arqueiro.addEventListener("click", () => alteraValorProfissao("Arqueiro"));
+function activateButton(clickedButton) {
+  for (var i = 0; i < characterButtons.length; i++) {
+    characterButtons[i].classList.remove('active');
+  }
+  clickedButton.classList.add('active');
+}
 
 var mainForm = document.querySelector(".mainForm");
 
 async function submitForm(e) {
-    e.preventDefault();
-    var nomePersonagem = document.querySelector("#name").value
-    var ataque = document.querySelector("#ataque").value
-    var vida = document.querySelector("#vida").value
-    var defesa = document.querySelector("#defesa").value
+  e.preventDefault();
+  var nomePersonagem = document.querySelector("#name").value;
+  var ataque = document.querySelector("#ataque").value;
+  var vida = document.querySelector("#vida").value;
+  var defesa = document.querySelector("#defesa").value;
 
-    var data = {
-        nomeReq: nomePersonagem,
-        profissao: tipoProfissao,
-        ataque: Number.parseInt(ataque),
-        vida: Number.parseInt(vida),
-        defesa: Number.parseInt(defesa)
-    };
+  var data = {
+    nomeReq: nomePersonagem,
+    profissao: tipoProfissao,
+    ataque: Number.parseInt(ataque),
+    vida: Number.parseInt(vida),
+    defesa: Number.parseInt(defesa)
+  };
 
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' }
-    }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' }
+  };
 
-    var response = await fetch("http://localhost:8080/session", options);
+  var response = await fetch("http://localhost:8080/session", options);
 
-    console.log(response);
+  console.log(response);
 }
 
 mainForm.addEventListener("submit", submitForm);
